e33fa2029515f4240b6579535bea1fae72de8be5
diegosalvador <diegodeazevedo28@gmail.com>
2023-09-28 14:40:09 -0400

    Initial commit

---
+++ imput.py
@@ -0,0 +1,9 @@
+nome  = input("informe o seu nome: ")
+idade = input("informe sua idade: ")
+
+
+print(nome, idade)
+print(nome, idade )
+print(nome, idade, end="... \n")
+print(nome, idade, sep="#", end="... \n")
+print(nome, idade, sep="#")
